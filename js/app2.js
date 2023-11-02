1e669f1c6054e26bc3658a40c42ba1c5617598a6
Rafaella Falcão <rafaella.maria@hotmail.com>
2023-11-01 21:37:18 -0300

    commit

--- js/app2.js
+++ js/app2.js
@@ -12,13 +12,37 @@ async function updateTable () {
         linhas +=`
         <tr>
             <td>${user.name}</td>
-            <td>${user.usuario}</td>
+            <td>${user.username}</td>
             <td>${user.email}</td>
             <td>
-                <button>exibir</button>
+                <button onclick= "userDetail(${user.id})">exibir</button>
             </td>
         </tr>`
 }
 tableBody.innerHTML = linhas
 }
+
+async function userDetail(userId) {
+    const resp = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    const user = await resp.json();
+
+    document.querySelector(".js-user-name").value = user.name
+    document.querySelector(".js-user-username").value = user.username
+}
+
+async function getTips() {
+    const resp = await fetch ("https://api.adviceslip.com/advice")
+    const tip = await resp.json()
+
+    setTimeout (()=>{
+        document.querySelector(".tips").innerText = tip.slip.advice
+        document.querySelector(".tips").style.display = "inline-block"
+    }, 3000)
+}
+
+setInterval (()=> {
+    getTips()
+    document.querySelector(".tips").style.display= "none"
+}, 8000)
+
 updateTable()
